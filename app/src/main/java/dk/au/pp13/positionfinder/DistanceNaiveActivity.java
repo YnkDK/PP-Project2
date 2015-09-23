2bb8512e6971e23b8c519cc8e0cf1dbf7a560fe9
Martin Storgaard <20140000@post.au.dk>
2015-09-23 16:59:17 +0200

    Distance naive done

--- app/src/main/java/dk/au/pp13/positionfinder/DistanceNaiveActivity.java
+++ app/src/main/java/dk/au/pp13/positionfinder/DistanceNaiveActivity.java
@@ -1,17 +1,45 @@
 package dk.au.pp13.positionfinder;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class DistanceNaiveActivity extends ActionBarActivity {
+    private TextView gpsCoordinates;
+    private boolean isGPSOn;
+    private LocationManager locationManager = null;
+    private GPSListener locationListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance_naive);
+
+        gpsCoordinates = (TextView) findViewById(R.id.gpsCoordinates);
+
+        gpsCoordinates.setText("GPS not started");
+        isGPSOn = false;
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new GPSListener(gpsCoordinates);
     }
 
+
+    public void toggleGPS(View view) {
+        if(isGPSOn && locationListener != null) {
+            locationManager.removeUpdates(locationListener);
+            gpsCoordinates.setText("GPS not started");
+        } else {
+            gpsCoordinates.setText("Waiting for GPS signal...");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                    locationListener);
+        }
+        isGPSOn = !isGPSOn;
+    }
 }
