8acfdb3e2b3f1c90f23a4a689c8e1c19f68d4f09
Casper <casperc91@gmail.com>
2015-09-23 12:16:27 +0200

    Update for task

--- app/src/main/java/dk/au/pp13/positionfinder/PeriodicActivity.java
+++ app/src/main/java/dk/au/pp13/positionfinder/PeriodicActivity.java
@@ -1,26 +1,24 @@
 package dk.au.pp13.positionfinder;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class PeriodicActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_periodic);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_periodic, menu);
         return true;
     }
 
@@ -38,10 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void periodic_reporting(View v) {
-        Intent myIntent = new Intent(this, PeriodicActivity.class);
-        this.startActivity(myIntent);
-    }
-
 }
